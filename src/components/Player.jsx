6569760d1a64e519bf56e2f69fff6ed26d360e56
jangsoyoon <1026jsy@gmail.com>
2024-04-02 13:59:42 +0900

    commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,17 +3,16 @@ export default function Player({name, symbol}){
     const [changeName, setChangeName] = useState(name);
     const [isEditing, setEditing] = useState(false);
     const handleEditClick = ()=>{
-        setEditing(prevState => !prevState)
-        let changeName = null;
-        if(isEditing){
-            changeName = document.getElementById("textField").value;
-            setChangeName(changeName);
-        }
+        setEditing(prevState => !prevState) 
+    }
+
+    const changeHandling = (e) =>{
+        setChangeName(e.target.value);
     }
     return (
         <li>
             <span className="player">
-             {isEditing ? <input id="textField" type="text" defaultValue={changeName} required/> :  <span className="player-name">{changeName}</span>}
+             {isEditing ? <input id="textField" type="text" value={changeName} onChange={changeHandling} required/> :  <span className="player-name">{changeName}</span>}
               <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
